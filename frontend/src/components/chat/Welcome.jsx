8c691f8fc617f5c140353b4f22a22f2eddf8c339
trollpy <tshivhenga18@gmail.com>
2025-07-11 01:24:28 +0200

    fix: case-sensitive import path for WelcomeSvg

--- frontend/src/components/chat/Welcome.jsx
+++ frontend/src/components/chat/Welcome.jsx
@@ -1,12 +1,11 @@
-import WelcomeSVG from "../../utils/WelcomeSvg";
+import { WelcomeSvg } from "../../utils/WelcomeSvg";
 
 export default function Welcome() {
   return (
     <div className="lg:col-span-2 lg:block bg-white dark:bg-gray-900">
       <div className="pl-5">
         <WelcomeSvg />
-        <div className="text-center">
-        </div>
+        <div className="text-center"></div>
       </div>
     </div>
   );
